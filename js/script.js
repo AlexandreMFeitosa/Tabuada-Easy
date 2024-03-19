c6a763d44a236528263e9aa72e47f4fd2aa5f89a
AlexandreMFeitosa <alexandremoraesfeitosa@gmail.com>
2024-03-18 22:56:21 -0300

    logica e validacao inseridos ok

--- js/script.js
+++ js/script.js
@@ -2,16 +2,24 @@
 
 function calcular () {
 
-    let numeroDaTabuada = document.querySelector("#tabuadaDo");
-    let numeroDaMultiplicacao = document.querySelector("#multiplicarAte");
-    let resultado = document.querySelector(".resultado");
+    let numeroDaTabuada = document.querySelector("#tabuadaDo").value ;
+        let numeroDaMultiplicacao = document.querySelector("#multiplicarAte").value ;
+        let resultado = document.querySelector("#resultado");
     
-    
-    for(let i = 0; i < numeroDaMultiplicacao; i++) {
-        
-        resultado = console.log(`${numeroDaTabuada} x ${i} = ${numeroDaTabuada * i}`);
+        let tabuada = ""
 
+    if(numeroDaMultiplicacao === '' || numeroDaTabuada === '') {
+        alert("Campo em branco, favor preenchar corretamente somente com numeros!")
+    } else {
+    
+        for(let i = 0; i <= numeroDaMultiplicacao; i++) {
+        
+            tabuada += `${numeroDaTabuada} x ${i} = ${numeroDaTabuada * i} <br>`;
+        }
+
+        resultado.innerHTML = tabuada ;
     }
-    resultado.innerHTML = `${resultado}`
+
+    
 
 }
